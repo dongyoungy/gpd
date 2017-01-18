9319cead269579a4b064f326d50d1b189d1a6b3f
Dong Young Yoon <dongyoungy@gmail.com>
2017-01-17 21:45:04 -0500

    testing

--- src/main/java/prelim/TempTest.java
+++ src/main/java/prelim/TempTest.java
@@ -1,6 +1,6 @@
 package prelim;
 
-import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.*;
 
 /**
  * Created by Dong Young Yoon on 1/17/17.
@@ -9,15 +9,18 @@ public class TempTest
 {
 	public static void main(String[] args)
 	{
-		String warehouseLocation = "spark-warehouse";
+		String warehouseLocation = "file:///spark-warehouse";
 		SparkSession spark = SparkSession
 				.builder()
 				.appName("dy test")
-				.config("spark.sql.warehouse.dir", warehouseLocation)
-				.enableHiveSupport()
+        .config("spark.sql.warehouse.dir", warehouseLocation)
+				//.config("hive.metastore.warehouse.dir", warehouseLocation)
+        .enableHiveSupport()
 				.getOrCreate();
 
-		spark.sql("create external table temp (string pageURL, int pageRank, int avgDuration) location '/data_gen/HiBench/Scan/Input/rankings'");
-		spark.sql("SELECT * from temp").show();
+		//spark.sql("create external table temp (string pageURL, int pageRank, int avgDuration) location '/data_gen/HiBench/Scan/Input/rankings'");
+		//spark.sql("SELECT * from temp").show();
+    Dataset<Row> df = spark.read().json("/home/dyoon/spark/examples/src/main/resources/people.json");
+    df.show();
 	}
 }
