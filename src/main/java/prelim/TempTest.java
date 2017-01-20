d476c4ebdb1513cab70764b93d65d498b183d97b
Dong Young Yoon <dongyoungy@gmail.com>
2017-01-20 11:48:01 -0500

    added test

--- src/main/java/prelim/TempTest.java
+++ src/main/java/prelim/TempTest.java
@@ -1,5 +1,6 @@
 package prelim;
 
+import java.util.*;
 import org.apache.spark.sql.*;
 
 /**
@@ -7,20 +8,76 @@ import org.apache.spark.sql.*;
  */
 public class TempTest
 {
-	public static void main(String[] args)
-	{
-		String warehouseLocation = "file:///spark-warehouse";
-		SparkSession spark = SparkSession
-				.builder()
-				.appName("dy test")
-        .config("spark.sql.warehouse.dir", warehouseLocation)
-				//.config("hive.metastore.warehouse.dir", warehouseLocation)
-        .enableHiveSupport()
-				.getOrCreate();
+    public static void main(String[] args)
+    {
+        String workload = args[0];
+        String dataSize = args[1];
+        String applicationName = workload + "_" + dataSize;
+        String inputData = String.format("/data_gen/HiBench/Data/%s/Input", dataSize);
+        String outputData = String.format("/data_gen/HiBench/Data/%s/Output", dataSize);
+        String warehouseLocation = "file:///spark-warehouse";
+        SparkSession spark = SparkSession
+            .builder()
+            .appName(applicationName)
+            .config("spark.sql.warehouse.dir", warehouseLocation)
+            .enableHiveSupport()
+            .getOrCreate();
 
-		//spark.sql("create external table temp (string pageURL, int pageRank, int avgDuration) location '/data_gen/HiBench/Scan/Input/rankings'");
-		//spark.sql("SELECT * from temp").show();
-    Dataset<Row> df = spark.read().json("/home/dyoon/spark/examples/src/main/resources/people.json");
-    df.show();
-	}
+        switch (workload)
+        {
+            case "Scan":
+            {
+                // setup queries
+                spark.sql("DROP TABLE IF EXISTS uservisits");
+                spark.sql(String.format("CREATE EXTERNAL TABLE uservisits (sourceIP STRING,destURL STRING,visitDate STRING,adRevenue DOUBLE,userAgent STRING,countryCode STRING,languageCode STRING,searchWord STRING,duration INT ) ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' STORED AS  SEQUENCEFILE LOCATION '%s/uservisits'", inputData));
+                spark.sql("DROP TABLE IF EXISTS uservisits_copy");
+                spark.sql(String.format("CREATE EXTERNAL TABLE uservisits_copy (sourceIP STRING,destURL STRING,visitDate STRING,adRevenue DOUBLE,userAgent STRING,countryCode STRING,languageCode STRING,searchWord STRING,duration INT ) ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' STORED AS  SEQUENCEFILE LOCATION '%s/uservisits_copy'", outputData));
+
+                // get plan
+                Dataset<Row> plan = spark.sql("EXPLAIN EXTENDED INSERT OVERWRITE TABLE uservisits_copy SELECT * FROM uservisits");
+                plan.write().mode("overwrite").text(String.format("/query_plans/%s/%s", workload, dataSize));
+                // run query
+                spark.sql("INSERT OVERWRITE TABLE uservisits_copy SELECT * FROM uservisits");
+
+                break;
+            }
+            case "Join":
+            {
+                // setup queries
+                spark.sql("DROP TABLE IF EXISTS rankings");
+                spark.sql(String.format("CREATE EXTERNAL TABLE rankings (pageURL STRING, pageRank INT, avgDuration INT) ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' STORED AS  SEQUENCEFILE LOCATION '%s/rankings'", inputData));
+                spark.sql("DROP TABLE IF EXISTS uservisits_copy");
+                spark.sql(String.format("CREATE EXTERNAL TABLE uservisits_copy (sourceIP STRING,destURL STRING,visitDate STRING,adRevenue DOUBLE,userAgent STRING,countryCode STRING,languageCode STRING,searchWord STRING,duration INT ) ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' STORED AS  SEQUENCEFILE LOCATION '%s/uservisits'", inputData));
+                spark.sql("DROP TABLE IF EXISTS rankings_uservisits_join");
+                spark.sql(String.format("CREATE EXTERNAL TABLE rankings_uservisits_join ( sourceIP STRING, avgPageRank DOUBLE, totalRevenue DOUBLE ) STORED AS  SEQUENCEFILE LOCATION '%s/rankings_uservisits_join'", outputData));
+
+                // get plan
+                Dataset<Row> plan = spark.sql("EXPLAIN EXTENDED INSERT OVERWRITE TABLE rankings_uservisits_join SELECT sourceIP, avg(pageRank), sum(adRevenue) as totalRevenue FROM rankings R JOIN (SELECT sourceIP, destURL, adRevenue FROM uservisits_copy UV WHERE (datediff(UV.visitDate, '1999-01-01')>=0 AND datediff(UV.visitDate, '2000-01-01')<=0)) NUV ON (R.pageURL = NUV.destURL) group by sourceIP order by totalRevenue DESC");
+                plan.write().mode("overwrite").text(String.format("/query_plans/%s/%s", workload, dataSize));
+                // run query
+                spark.sql("INSERT OVERWRITE TABLE rankings_uservisits_join SELECT sourceIP, avg(pageRank), sum(adRevenue) as totalRevenue FROM rankings R JOIN (SELECT sourceIP, destURL, adRevenue FROM uservisits_copy UV WHERE (datediff(UV.visitDate, '1999-01-01')>=0 AND datediff(UV.visitDate, '2000-01-01')<=0)) NUV ON (R.pageURL = NUV.destURL) group by sourceIP order by totalRevenue DESC");
+                break;
+            }
+            case "Aggregation":
+            {
+                // setup queries
+                spark.sql("DROP TABLE IF EXISTS uservisits");
+                spark.sql(String.format("CREATE EXTERNAL TABLE uservisits (sourceIP STRING,destURL STRING,visitDate STRING,adRevenue DOUBLE,userAgent STRING,countryCode STRING,languageCode STRING,searchWord STRING,duration INT ) ROW FORMAT SERDE 'org.apache.hadoop.hive.serde2.OpenCSVSerde' STORED AS  SEQUENCEFILE LOCATION '%s/uservisits'", inputData));
+                spark.sql("DROP TABLE IF EXISTS uservisits_aggre");
+                spark.sql(String.format("CREATE EXTERNAL TABLE uservisits_aggre ( sourceIP STRING, sumAdRevenue DOUBLE ) STORED AS SEQUENCEFILE LOCATION '%s/uservisits_aggre'", outputData));
+
+                // get plan
+                Dataset<Row> plan = spark.sql("EXPLAIN EXTENDED INSERT OVERWRITE TABLE uservisits_aggre SELECT sourceIP, SUM(adRevenue) FROM uservisits GROUP BY sourceIP");
+                plan.write().mode("overwrite").text(String.format("/query_plans/%s/%s", workload, dataSize));
+
+                // run query
+                spark.sql("INSERT OVERWRITE TABLE uservisits_aggre SELECT sourceIP, SUM(adRevenue) FROM uservisits GROUP BY sourceIP");
+                break;
+            }
+            default:
+                return;
+        }
+
+        spark.stop();
+    }
 }
