b47666ebf34a7b0d1f3c1a9fc74307e9099b53d1
Dong Young Yoon <dongyoungy@gmail.com>
2017-03-09 13:36:06 -0500

    changing parameter for SMOreg.

--- src/main/java/edu/umich/gpd/algorithm/ILPSolver2.java
+++ src/main/java/edu/umich/gpd/algorithm/ILPSolver2.java
@@ -164,7 +164,7 @@ public class ILPSolver2 extends AbstractSolver {
         smo.setOptions(Utils.splitOptions("-C 1.0 -N 0 " +
             "-I \"weka.classifiers.functions.supportVector.RegSMOImproved " +
             "-T 0.001 -V -P 1.0E-12 -L 0.001 -W 1\" " +
-            "-K \"weka.classifiers.functions.supportVector.PolyKernel -E 1.0 -C 250007\""));
+            "-K \"weka.classifiers.functions.supportVector.PolyKernel -E 1.0 -C 0\""));
         m5p.setOptions(Utils.splitOptions("-R -M 1"));
       } catch (Exception e) {
         GPDLogger.error(this, "Failed to set options for the classifier.");
@@ -323,7 +323,7 @@ public class ILPSolver2 extends AbstractSolver {
       smo.setOptions(Utils.splitOptions("-C 1.0 -N 0 " +
           "-I \"weka.classifiers.functions.supportVector.RegSMOImproved " +
           "-T 0.001 -V -P 1.0E-12 -L 0.001 -W 1\" " +
-          "-K \"weka.classifiers.functions.supportVector.PolyKernel -E 1.0 -C 250007\""));
+          "-K \"weka.classifiers.functions.supportVector.PolyKernel -E 1.0 -C 0\""));
     } catch (Exception e) {
       GPDLogger.error(this, "Failed to set options for the classifier.");
       e.printStackTrace();
