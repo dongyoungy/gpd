e94475dd7a77f7f39ea53729e22506c3706b908a
Dong Young Yoon <dongyoungy@gmail.com>
2017-12-06 17:42:20 -0500

    test

--- src/main/java/edu/umich/gpd/algorithm/SASolver.java
+++ src/main/java/edu/umich/gpd/algorithm/SASolver.java
@@ -137,11 +137,12 @@ public class SASolver extends AbstractSolver {
       double sizeDiff, double timeDiff, double currentTemp, double targetTemp) {
     if (sizeDiff < 0 && timeDiff < 0) return 1.0;
     else if (sizeDiff * timeDiff < 0) {
-      if (sizeDiff < 0) {
-        return Math.exp((sizeDiff - (2 * timeDiff)) / (currentTemp / (10 * targetTemp)));
-      } else {
-        return Math.exp((timeDiff - (2 * sizeDiff)) / (currentTemp / (10 * targetTemp)));
-      }
+      return Math.exp((sizeDiff * timeDiff)) / (currentTemp / (10 * targetTemp));
+//      if (sizeDiff < 0) {
+//        return Math.exp((sizeDiff - (2 * timeDiff)) / (currentTemp / (10 * targetTemp)));
+//      } else {
+//        return Math.exp((timeDiff - (2 * sizeDiff)) / (currentTemp / (10 * targetTemp)));
+//      }
     } else {
       return Math.exp((-5 * (timeDiff + sizeDiff))) / ((currentTemp / (10 * targetTemp)));
     }
